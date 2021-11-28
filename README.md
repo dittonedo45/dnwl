d88b2be903e92abf7501ae46e96b26c702155523
Eddington Chamunorwa <dittonedo45@github.com>
2021-11-28 16:10:18 +0200

    Improved Demostration.

--- README.md
+++ README.md
@@ -6,6 +6,72 @@ An Example of email forger, worlist generator. Take the idea and use it to your 
 ## Examples, 
  `make`
  `dnwl foo bar`
+# Idea
+```C
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-### Image Illustration
-![example image](./example.png)
+typedef struct node node;
+struct node {
+    int l;
+    node *n;
+};
+
+node *n_create()
+{
+    node *a = malloc(sizeof(node));
+    a->l = 0;
+    a->n = 0;
+
+    return a;
+}
+
+void n_gen(node ** p, int m)
+{
+    node **r = p;
+    if (!r)
+	return;
+
+    while (1) {
+	if (!*r) {
+	    *r = n_create();
+	    break;
+	} else if ((*r)->l < m) {
+	    (*r)->l++;
+	    break;
+	} else if ((*r)->l >= m) {
+	    (*r)->l = 0;
+	    r = &(*r)->n;
+	}
+    }
+}
+
+void n_puts(void **data, int n)
+{
+    printf("%s", data[n]);
+}
+
+void n_iter(node * r, void (*my_func) (void **data, int n), void **data)
+{
+    if (!r)
+	return;
+    my_func(data, r->l);
+    if (!r->n) {
+	printf("%s\n", "");
+    }
+    n_iter(r->n, my_func, data);
+}
+
+int main(signed Argsc, char *(Args[]))
+{
+
+    node *f = n_create();
+
+    while (1) {
+	n_iter(f, n_puts, (void **) Args + 1);
+	n_gen(&f, Argsc - 2);
+    }
+    return 0;
+}
+```
--- main.c
+++ main.c
@@ -5,63 +5,63 @@
 
 typedef struct node node;
 struct node {
- int l;
- node *n;
+    int l;
+    node *n;
 };
 
-node *n_create (  )
+node *n_create()
 {
- node *a = malloc ( sizeof ( node ) );
- a->l = 0;
- a->n = 0;
+    node *a = malloc(sizeof(node));
+    a->l = 0;
+    a->n = 0;
 
- return a;
+    return a;
 }
 
-void n_gen ( node ** p, int m )
+void n_gen(node ** p, int m)
 {
- node **r = p;
- if( !r )
-  return;
+    node **r = p;
+    if (!r)
+	return;
 
- while( 1 ) {
-  if( !*r ) {
-   *r = n_create (  );
-   break;
-  } else if( ( *r )->l < m ) {
-   ( *r )->l++;
-   break;
-  } else if( ( *r )->l >= m ) {
-   ( *r )->l = 0;
-   r = &( *r )->n;
-  }
- }
+    while (1) {
+	if (!*r) {
+	    *r = n_create();
+	    break;
+	} else if ((*r)->l < m) {
+	    (*r)->l++;
+	    break;
+	} else if ((*r)->l >= m) {
+	    (*r)->l = 0;
+	    r = &(*r)->n;
+	}
+    }
 }
 
-void n_puts ( void **data, int n )
+void n_puts(void **data, int n)
 {
- printf ( "%s", data[n] );
+    printf("%s", data[n]);
 }
 
-void n_iter ( node * r, void ( *my_func ) ( void **data, int n ), void **data )
+void n_iter(node * r, void (*my_func) (void **data, int n), void **data)
 {
- if( !r )
-  return;
- my_func ( data, r->l );
- if( !r->n ) {
-  printf ( "%s\n", "@gmail.com" );
- }
- n_iter ( r->n, my_func, data );
+    if (!r)
+	return;
+    my_func(data, r->l);
+    if (!r->n) {
+	printf("%s\n", "");
+    }
+    n_iter(r->n, my_func, data);
 }
 
-int main ( signed Argsc, char *( Args[] ) )
+int main(signed Argsc, char *(Args[]))
 {
 
- node *f = n_create (  );
+    node *f = n_create();
 
- while( 1 ) {
-  n_iter ( f, n_puts, ( void ** )Args + 1 );
-  n_gen ( &f, Argsc - 2 );
- }
- return 0;
+    while (1) {
+	n_iter(f, n_puts, (void **) Args + 1);
+	n_gen(&f, Argsc - 2);
+    }
+    return 0;
 }
